b4b69c9496d26ad88b55682825d3b615b8e5ef38
HengLi <heeric891120@gmail.com>
2025-05-01 01:07:26 +0800

    刪除測試api

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import linebot from 'linebot'
 import flexText from './commands/flexText.js'
 import flexLocation from './commands/flexLocation.js'
 import flexTextDay from './commands/flexTextDay.js'
-import axios from 'axios'
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -24,16 +23,5 @@ bot.on('message', event => {
 })
 
 bot.listen('/', process.env.PORT || 3000, () => {
-  testApi()
   console.log('啟動')
 })
-
-const testApi = async () => {
-  try {
-    await axios.get('https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-003?Authorization=CWA-3538B9F3-1190-4861-8523-3E66C9639ED8&ElementName=%E5%A4%A9%E6%B0%A3%E9%A0%90%E5%A0%B1%E7%B6%9C%E5%90%88%E6%8F%8F%E8%BF%B0,%E5%A4%A9%E6%B0%A3%E7%8F%BE%E8%B1%A1')
-    console.log('測試api成功')
-  } catch (err) {
-    console.log('測試api失敗')
-    console.log(err)
-  }
-}
